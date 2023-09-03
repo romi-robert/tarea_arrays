fce17bc096260981dab201e2b54edb2820c4c55b
Romina Robert <140982148+romi-robert@users.noreply.github.com>
2023-09-03 12:43:37 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "B is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+  document.addEventListener("DOMContentLoaded", (e) => {
+    // Filtrar los elementos de tipo String
+    const filteredArray = strangeArray.filter((element) => typeof element === "string");
+  
+    // Ordenar alfabéticamente
+    filteredArray.sort();
+  
+    // Mostrar el array filtrado y ordenado en la lista
+    showList(filteredArray);
+  });
